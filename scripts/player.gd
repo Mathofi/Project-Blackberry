55ce3dfa1fa186651e247a92272fb6a809e1b12b
Isaac <issebaske@gmail.com>
2025-09-13 14:08:19 -0600

    added better movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,24 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 100.0
+@export var SPEED = 200.0 # max speed 
+const ACCELERATOIN = 1000.0
+const FRICTON = 800.0
 
-
+## movement
 func _physics_process(delta: float) -> void:
+	# vector 2 is for bolth axes for (0,0)
+	var input_vector = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	).normalized()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x_direction := Input.get_axis("move_left", "move_right")
-	if x_direction:
-		velocity.x = x_direction * SPEED
+	if input_vector != Vector2.ZERO:
+		# accelerat toward the directon 
+		var target = input_vector * SPEED
+		velocity = velocity.move_toward(target, ACCELERATOIN * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var y_direction := Input.get_axis("move_up", "move_down")
-	if y_direction:
-		velocity.y = y_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.x, 0, SPEED)
-
+		# moves twords 0 then no input
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTON * delta)
 
 	move_and_slide()
